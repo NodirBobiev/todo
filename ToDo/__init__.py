72cb31c65c3273e80d69905d3a2cf0938f38ed57
NodirBobiev <nodir.bobiev@gmail.com>
2022-02-16 21:21:32 +0300

    add authentication

--- ToDo/__init__.py
+++ ToDo/__init__.py
@@ -4,7 +4,11 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY = 'dev')
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE=os.path.join(app.instance_path, "ToDo.sqlite")
+    )
+
     if test_config is None:
         app.config.from_pyfile("config.py", silent = True)
     else:
@@ -14,6 +18,15 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import team
+    app.register_blueprint(team.bp)
 
     @app.route("/hello")
     def hello():
